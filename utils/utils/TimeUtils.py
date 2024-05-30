0909a284f4b0a2e7504ab12145b69b9e28bac517
JiJiBo <38462693+JiJiBo@users.noreply.github.com>
2024-05-30 20:17:56 +0800

    写转换部分

--- utils/utils/TimeUtils.py
+++ utils/utils/TimeUtils.py
@@ -4,9 +4,11 @@ import datetime
 def time_to_str(time):
     if time.days > 0:
         return f"{time.days}天{time.seconds // 3600}小时{time.seconds % 3600 // 60}分钟{time.seconds % 60}秒"
-    elif time.seconds > 0:
+    elif time.seconds // 3600 > 0:
         return f"{time.seconds // 3600}小时{time.seconds % 3600 // 60}分钟{time.seconds % 60}秒"
-    elif time.seconds > 0:
+    elif time.seconds // 60 > 0:
+        return f"{time.seconds // 60}分钟{time.seconds % 60}秒"
+    else:
         return f"{time.seconds}秒"
 
 
