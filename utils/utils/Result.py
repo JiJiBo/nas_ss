d3bbc34257b5cd1b262ba7cae9ae475c386dbead
JiJiBo <38462693+JiJiBo@users.noreply.github.com>
2024-05-31 11:34:00 +0800

    丰富接口

--- utils/utils/Result.py
+++ utils/utils/Result.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
 
-
+headers = {"Access-Control-Allow-Origin": "*"}
 def getErrorResult(error, code=500):
-    return JsonResponse({'error': error, 'code': code, 'msg': 'error'}, status=200)
+    return JsonResponse({'error': error, 'code': code, 'msg': 'error'}, status=200,headers=headers)
 
 
 def getOkResult(data):
-    return JsonResponse({"data": data, 'code': 200, 'msg': 'success'}, status=200)
+    return JsonResponse({"data": data, 'code': 200, 'msg': 'success'}, status=200,headers=headers)
